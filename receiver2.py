9d5db78fc24471d895fa21d63a4026b29111933d
Tian Xin <txin@cmu.edu>
2015-12-07 23:19:19 +0000

    increased threshold for receiver

--- receiver2.py
+++ receiver2.py
@@ -35,7 +35,7 @@ print >> sys.stderr,  time.time() - baselineStartingTime
 
 baseline /= counter
 print >> sys.stderr,  'counter: ', counter
-threshold = baseline * 1.35
+threshold = baseline * 1.8
 print >> sys.stderr,  'receiver baseline: ', baseline
 print >> sys.stderr,  'threshold', threshold
 
